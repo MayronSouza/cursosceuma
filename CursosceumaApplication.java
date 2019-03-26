f104eae0515a92a62983d1f48ae5864e308c0735
MayronSouza <mayrondrummer@gmail.com>
2019-03-25 22:56:04 -0300

    Incluindo Clase que contem o main

---
+++ CursosceumaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursosceuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosceumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosceumaApplication.class, args);
+	}
+
+}
